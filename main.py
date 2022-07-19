a9be170d39a2b5676e50c97944799bda2626d994
Konstantinos Prasinos <konstantinos.prasinos@gmail.com>
2022-07-19 19:59:04 +0300

    Finish generator

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+from os import remove
 import numpy as np
+import random
+import copy
 
-# table = [[0]*9 for i in range(9)]
-table = [
-[0, 0, 5, 6, 0, 2, 0, 0, 0],
-[7, 0, 0, 8, 1, 0, 4, 0, 0],
-[0, 0, 0, 0, 0, 0, 5, 6, 0],
-[0, 4, 9, 0, 0, 0, 0, 0, 0],
-[8, 0, 0, 0, 0, 0, 0, 0, 7],
-[0, 0, 0, 0, 5, 1, 0, 0, 0],
-[0, 0, 0, 9, 4, 0, 8, 0, 2],
-[3, 0, 6, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
-validPositions = 0
+table = [[0]*9 for i in range(9)]
+# table = [
+# [0, 0, 5, 6, 0, 2, 0, 0, 0],
+# [7, 0, 0, 8, 1, 0, 4, 0, 0],
+# [0, 0, 0, 0, 0, 0, 5, 6, 0],
+# [0, 4, 9, 0, 0, 0, 0, 0, 0],
+# [8, 0, 0, 0, 0, 0, 0, 0, 7],
+# [0, 0, 0, 0, 5, 1, 0, 0, 0],
+# [0, 0, 0, 9, 4, 0, 8, 0, 2],
+# [3, 0, 6, 1, 0, 0, 0, 0, 0],
+# [0, 0, 0, 0, 0, 0, 0, 0, 0]
+# ]
 
 def printTable(table):
     for rowIndex, row in enumerate(table):
@@ -21,7 +23,7 @@ def printTable(table):
         for columnIndex, tile in enumerate(row):
             if columnIndex % 3 == 0:
                 print("|", end = ' ')
-            print(f' {tile} ', end = ' ')
+            print(f' {tile if tile != 0 else " "} ', end = ' ')
         print("|")
     print("+-------------+-------------+-------------+")
 
@@ -55,6 +57,13 @@ def findUnfilledtile(table):
                 return x, y
     return -1, -1
 
+
+def removeRandomTiles(table, numberToRemove):
+    for i in range(numberToRemove):
+        x = random.randint(0, 8)
+        y = random.randint(0, 8)
+        table[y][x] = 0
+
 def fillTable(table):
     tableCopy = table.copy()
     for rowIndex, row in enumerate(tableCopy):
@@ -74,19 +83,29 @@ def fillTable(table):
 
 def solveSudoku(table):
     x, y = findUnfilledtile(table)
+    unusedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     
     if y == -1: 
         return True
     
-    for value in range(1, 10):
+    while len(unusedNumbers) != 0:
+        value = unusedNumbers[random.randint(0, len(unusedNumbers) - 1)]
+        unusedNumbers.remove(value)
+
         if checkPosition(table, x, y, value):
             table[y][x] = value
             if solveSudoku(table):
                 return True
             table[y][x] = 0
     return False
-        
 
-# fillTable(table)
 solveSudoku(table)
+completeSudoku = copy.deepcopy(table)
+solvable = False
+
+while (solvable == False):
+    table = copy.deepcopy(completeSudoku)
+    removeRandomTiles(table, 49)
+    solvable = solveSudoku(copy.deepcopy(table))
+
 printTable(table)
